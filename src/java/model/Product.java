33e3a15f1cc1503ee520dfba40c8406aac61220a
thanhan25052003 <thanhan01236339441@gmail.com>
2024-03-02 04:41:24 +0700

    init ProductDAO and CategoryDAO

---
+++ src/java/model/Product.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author admin
+ */
+@ToString
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    private int id;
+    private String name;
+    private String image;
+    private int quantity;
+    private float price;
+    private String description;
+    private int categoryId;
+
+}
